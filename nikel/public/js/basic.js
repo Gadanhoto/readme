cc4bb2ec552ae991a606f9374a68f14b26cb7911
Rodrigo Gadanhoto <126842239+Gadanhoto@users.noreply.github.com>
2023-03-08 18:24:13 -0300

    Add files via upload

---
+++ nikel/public/js/basic.js
@@ -0,0 +1,37 @@
+const nome ="Rodrigo Gadanhoto"
+let nome2 ="rodrigo"
+let pessoa = {
+    nome: "Rodrigo Gadanhoto",
+    Idade: "33",
+    Trabalho: "sem trabalho",
+}
+let nomes = ["Rodrigo Gadanhoto, Chico Bento, antoio"];
+let pessoas = [];
+
+
+
+function alterarNnome(){
+    nome2="Elvis";
+    console.log("valor alterado:");
+    console.log(nome2);
+}
+
+function recebeEalteraNome(novoNome){
+    nome2 = novoNome;
+    console.log("valor alterado recebendo nome:");
+    console.log(nome2);
+}
+
+function adicionarPessoa(pessoa);
+pessoa.push(pessoa);
+
+adicionarPessoa({
+    nome: "Pedro Silva",
+    Idade: "28",
+    trabalho: "porteiro",
+})
+
+console.log(pessoas);
+
+
+//alteraNome();
